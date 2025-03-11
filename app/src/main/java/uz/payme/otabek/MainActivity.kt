d2c97dab6cc8150620f0e9ecd75b853e26e0c740
Otabek <otabek.n@payme.uz>
2025-03-11 19:03:50 +0500

    added viewmodel

--- app/src/main/java/uz/payme/otabek/MainActivity.kt
+++ app/src/main/java/uz/payme/otabek/MainActivity.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import uz.payme.otabek.ui.theme.PaymeStopwatchTheme
 
